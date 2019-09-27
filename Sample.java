4d5774869cd6114a615ea6f78b4cd27ba6df7937
Fakrudeen Shahul <fakrudeen@github.com>
2019-09-27 09:08:53 +0530

    my commit message

---
+++ Sample.java
@@ -0,0 +1,29 @@
+public class Sample {
+
+    private RemoteWebDriver driver;
+
+    public Sample(RemoteWebDriver driver){
+        this.driver = driver;
+    }
+
+
+    /*PO
+    {
+        "title":"Sample",
+        "info":[
+        {"value":[{"class":"Button","frameName":"","locator":{"tag_name":"INPUT","type":"name","value":"name=btnK"},"name":"tablet","strict_locator":"name=btnK"}]}
+        
+
+        ]
+    }
+
+    PO*/
+
+    private By tablet = By.name("name=btnK");  
+      
+
+    public RemoteWebElement gettablet(){
+        return driver.findElement(tablet);
+    }      
+}
+
