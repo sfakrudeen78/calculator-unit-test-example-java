09bbff3495c0255e40ef49f6655ae3ec4a18c012
Fakrudeen Shahul <fakrudeen@github.com>
2019-09-29 13:49:51 +0530

    my commit message

--- Sample.java
+++ Sample.java
@@ -13,6 +13,8 @@ public class Sample {
         "info":[
         {"key":"junga","value":[{"class":"Button","frameName":"","locator":{"tag_name":"INPUT","type":"name","value":"name=btnK"},"name":"junga","strict_locator":"name=btnK"}]}
         
+,        {"key":"monkye","value":[{"class":"TextField","frameName":"","locator":{"tag_name":"INPUT","type":"name","value":"name=q"},"name":"monkye","strict_locator":"name=q"}]}
+        
 
         ]
     }
@@ -21,9 +23,14 @@ public class Sample {
 
     private By junga = By.name("name=btnK");  
       
+,    private By monkye = By.name("name=q");  
+      
 
     public RemoteWebElement getjunga(){
         return driver.findElement(junga);
+    }      
+,    public RemoteWebElement getmonkye(){
+        return driver.findElement(monkye);
     }      
 }
 
