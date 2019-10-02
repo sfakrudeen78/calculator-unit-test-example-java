39c47eaf06258b2c539ff809ba06102b111844d6
sfakrudeen78 <fakrudeen@gmail.com>
2019-10-02 10:49:24 +0530

    initial commit

--- Twinkle.java
+++ Twinkle.java
@@ -9,36 +9,21 @@ public class Twinkle {
 
     /*PO
     {
-        "metadata":"%5B%7B%22key%22%3A%22tab%22%2C%22value%22%3A%5B%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23topnav%20%3E%20div%20%3E%20div.w3-bar.w3-left%20%3E%20a.w3-bar-item.w3-button%22%7D%2C%22name%22%3A%22tab%22%2C%22strict_locator%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23topnav%20%3E%20div%20%3E%20div.w3-bar.w3-left%20%3E%20a.w3-bar-item.w3-button%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//div%5B@id%3D%27topnav%27%5D/div/div/a%5B10%5D%22%7D%2C%22name%22%3A%22tab%22%2C%22strict_locator%22%3A%22//div%5B@id%3D%27topnav%27%5D/div/div/a%5B10%5D%22%7D%5D%7D%2C%7B%22key%22%3A%22mango%22%2C%22value%22%3A%5B%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Da.w3-btn.w3-margin-bottom%22%7D%2C%22name%22%3A%22mango%22%2C%22strict_locator%22%3A%22css%3Da.w3-btn.w3-margin-bottom%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//div%5B@id%3D%27main%27%5D/div%5B2%5D/a%22%7D%2C%22name%22%3A%22mango%22%2C%22strict_locator%22%3A%22//div%5B@id%3D%27main%27%5D/div%5B2%5D/a%22%7D%5D%7D%2C%7B%22key%22%3A%22gambling%22%2C%22value%22%3A%5B%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23belowtopnav%20%3E%20div.w3-row.w3-white%20%3E%20%23main%20%3E%20p%20%3E%20a.w3-btn.w3-white%20w3-border%22%7D%2C%22name%22%3A%22gambling%22%2C%22strict_locator%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23belowtopnav%20%3E%20div.w3-row.w3-white%20%3E%20%23main%20%3E%20p%20%3E%20a.w3-btn.w3-white%20w3-border%22%7D%5D%7D%2C%7B%22key%22%3A%22screenx%22%2C%22value%22%3A%5B%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23sidenav%20%3E%20%23leftmenuinner%20%3E%20%23leftmenuinnerinner%20%3E%20div.notranslate%20%3E%20div.ref_overview%20%3E%20a%3Anth-of-type%2823%29%22%7D%2C%22name%22%3A%22screenx%22%2C%22strict_locator%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23sidenav%20%3E%20%23leftmenuinner%20%3E%20%23leftmenuinnerinner%20%3E%20div.notranslate%20%3E%20div.ref_overview%20%3E%20a%3Anth-of-type%2823%29%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//div%5B@id%3D%27leftmenuinnerinner%27%5D/div/div%5B25%5D/a%5B24%5D%22%7D%2C%22name%22%3A%22screenx%22%2C%22strict_locator%22%3A%22//div%5B@id%3D%27leftmenuinnerinner%27%5D/div/div%5B25%5D/a%5B24%5D%22%7D%5D%7D%2C%7B%22key%22%3A%22stop%22%2C%22value%22%3A%5B%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23sidenav%20%3E%20%23leftmenuinner%20%3E%20%23leftmenuinnerinner%20%3E%20div.notranslate%20%3E%20div.ref_overview%20%3E%20a%3Anth-of-type%2822%29%22%7D%2C%22name%22%3A%22stop%22%2C%22strict_locator%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23sidenav%20%3E%20%23leftmenuinner%20%3E%20%23leftmenuinnerinner%20%3E%20div.notranslate%20%3E%20div.ref_overview%20%3E%20a%3Anth-of-type%2822%29%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//a%5Bcontains%28text%28%29%2C%27screenTop%27%29%5D%22%7D%2C%22name%22%3A%22stop%22%2C%22strict_locator%22%3A%22//a%5Bcontains%28text%28%29%2C%27screenTop%27%29%5D%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//div%5B@id%3D%27leftmenuinnerinner%27%5D/div/div%5B25%5D/a%5B23%5D%22%7D%2C%22name%22%3A%22stop%22%2C%22strict_locator%22%3A%22//div%5B@id%3D%27leftmenuinnerinner%27%5D/div/div%5B25%5D/a%5B23%5D%22%7D%5D%7D%5D"
+        "metadata":"%5B%7B%22key%22%3A%22tab%22%2C%22value%22%3A%5B%7B%22class%22%3A%22H1%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22H1%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Dh1.post-title%22%7D%2C%22name%22%3A%22tab%22%2C%22strict_locator%22%3A%22css%3Dh1.post-title%22%7D%2C%7B%22class%22%3A%22H1%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22H1%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//div%5B@id%3D%27container%27%5D/div/article/header/h1%22%7D%2C%22name%22%3A%22tab%22%2C%22strict_locator%22%3A%22//div%5B@id%3D%27container%27%5D/div/article/header/h1%22%7D%5D%7D%2C%7B%22key%22%3A%22tabeee%22%2C%22value%22%3A%5B%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23wrapper%20%3E%20%23container%20%3E%20div.container.single-post-container%20has-sidebar%20%3E%20aside%20%3E%20ul%20%3E%20li%3Anth-of-type%285%29%20%3E%20a%22%7D%2C%22name%22%3A%22tabeee%22%2C%22strict_locator%22%3A%22css%3Dhtml%20%3E%20body%20%3E%20%23wrapper%20%3E%20%23container%20%3E%20div.container.single-post-container%20has-sidebar%20%3E%20aside%20%3E%20ul%20%3E%20li%3Anth-of-type%285%29%20%3E%20a%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//a%5Bcontains%28text%28%29%2C%27ES5%20to%20ESNext%27%29%5D%22%7D%2C%22name%22%3A%22tabeee%22%2C%22strict_locator%22%3A%22//a%5Bcontains%28text%28%29%2C%27ES5%20to%20ESNext%27%29%5D%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//div%5B@id%3D%27container%27%5D/div/aside/ul/li%5B6%5D/a%22%7D%2C%22name%22%3A%22tabeee%22%2C%22strict_locator%22%3A%22//div%5B@id%3D%27container%27%5D/div/aside/ul/li%5B6%5D/a%22%7D%2C%7B%22class%22%3A%22A%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22A%22%2C%22type%22%3A%22xpath%22%2C%22value%22%3A%22//a%5Bcontains%28@href%2C%20%27/page/es5-to-esnext%27%29%5D%22%7D%2C%22name%22%3A%22tabeee%22%2C%22strict_locator%22%3A%22//a%5Bcontains%28@href%2C%20%27/page/es5-to-esnext%27%29%5D%22%7D%5D%7D%5D"
     }
 
     PO*/
 
-    private By tab = By.css("css=html > body > #topnav > div > div.w3-bar.w3-left > a.w3-bar-item.w3-button");  
+    private By tab = By.css("css=h1.post-title");  
       
-,    private By mango = By.css("css=a.w3-btn.w3-margin-bottom");  
-      
-,    private By gambling = By.css("css=html > body > #belowtopnav > div.w3-row.w3-white > #main > p > a.w3-btn.w3-white w3-border");  
-      
-,    private By screenx = By.css("css=html > body > #sidenav > #leftmenuinner > #leftmenuinnerinner > div.notranslate > div.ref_overview > a:nth-of-type(23)");  
-      
-,    private By stop = By.css("css=html > body > #sidenav > #leftmenuinner > #leftmenuinnerinner > div.notranslate > div.ref_overview > a:nth-of-type(22)");  
+    private By tabeee = By.css("css=html > body > #wrapper > #container > div.container.single-post-container has-sidebar > aside > ul > li:nth-of-type(5) > a");  
       
 
     public RemoteWebElement gettab(){
         return driver.findElement(tab);
     }      
-,    public RemoteWebElement getmango(){
-        return driver.findElement(mango);
-    }      
-,    public RemoteWebElement getgambling(){
-        return driver.findElement(gambling);
-    }      
-,    public RemoteWebElement getscreenx(){
-        return driver.findElement(screenx);
-    }      
-,    public RemoteWebElement getstop(){
-        return driver.findElement(stop);
+    public RemoteWebElement gettabeee(){
+        return driver.findElement(tabeee);
     }      
 }
 
