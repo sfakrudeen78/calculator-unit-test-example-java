670ed5444097e80fa9fa1fad815ed3d497b0b56c
Fakrudeen Shahul <fakrudeen@gmail.com>
2019-10-05 10:59:14 +0530

    test commit

---
+++ src/main/java/com/pageobjects/test/Tester1.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Tester1.java {
+
+    private RemoteWebDriver driver;
+
+    public Tester1.java(RemoteWebDriver driver){
+        this.driver = driver;
+    }
+
+
+    //PAGE-OBJECT-START
+
+    /*PO
+    {
+        "metadata":"%5B%7B%22key%22%3A%22buster%22%2C%22value%22%3A%5B%7B%22class%22%3A%22Button%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22INPUT%22%2C%22type%22%3A%22name%22%2C%22value%22%3A%22name%3DbtnK%22%7D%2C%22name%22%3A%22buster%22%2C%22page_verification%22%3Afalse%2C%22strict_locator%22%3A%22name%3DbtnK%22%7D%2C%7B%22class%22%3A%22Button%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22INPUT%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Ddiv.FPdoLc.VlcLAe%20%3E%20center%20%3E%20input%5Bname%3D%5C%22btnK%5C%22%5D%22%7D%2C%22name%22%3A%22buster%22%2C%22page_verification%22%3Afalse%2C%22strict_locator%22%3A%22css%3Ddiv.FPdoLc.VlcLAe%20%3E%20center%20%3E%20input%5Bname%3D%5C%22btnK%5C%22%5D%22%7D%5D%7D%5D"
+    }
+
+    PO*/
+
+    
+    private By buster = By.name("name=btnK");  
+      
+
+    public RemoteWebElement getBuster(){
+        return driver.findElement(buster);
+    }      
+
+    //PAGE-OBJECT-END
+
+
+
+}
+
