7cb59974ab9f0d18ca9e813fcdd8cf316cad4157
Fakrudeen Shahul <fakrudeen@gmail.com>
2019-10-05 10:58:09 +0530

    test commit

--- src/main/java/com/pageobjects/test/Tester.java
+++ src/main/java/com/pageobjects/test/Tester.java
@@ -15,20 +15,21 @@ public class Tester.java {
 
     /*PO
     {
-        "metadata":"%5B%7B%22key%22%3A%22test%22%2C%22value%22%3A%5B%7B%22class%22%3A%22Button%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22INPUT%22%2C%22type%22%3A%22name%22%2C%22value%22%3A%22name%3DbtnK%22%7D%2C%22name%22%3A%22test%22%2C%22page_verification%22%3Afalse%2C%22strict_locator%22%3A%22name%3DbtnK%22%7D%2C%7B%22class%22%3A%22Button%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22INPUT%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Ddiv.FPdoLc.VlcLAe%20%3E%20center%20%3E%20input%5Bname%3D%5C%22btnK%5C%22%5D%22%7D%2C%22name%22%3A%22test%22%2C%22page_verification%22%3Afalse%2C%22strict_locator%22%3A%22css%3Ddiv.FPdoLc.VlcLAe%20%3E%20center%20%3E%20input%5Bname%3D%5C%22btnK%5C%22%5D%22%7D%5D%7D%5D"
+        "metadata":"%5B%7B%22key%22%3A%22mango%22%2C%22value%22%3A%5B%7B%22class%22%3A%22Button%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22INPUT%22%2C%22type%22%3A%22name%22%2C%22value%22%3A%22name%3DbtnK%22%7D%2C%22name%22%3A%22mango%22%2C%22page_verification%22%3Afalse%2C%22strict_locator%22%3A%22name%3DbtnK%22%7D%2C%7B%22class%22%3A%22Button%22%2C%22frameName%22%3A%22%22%2C%22locator%22%3A%7B%22tag_name%22%3A%22INPUT%22%2C%22type%22%3A%22css%22%2C%22value%22%3A%22css%3Ddiv.FPdoLc.VlcLAe%20%3E%20center%20%3E%20input%5Bname%3D%5C%22btnK%5C%22%5D%22%7D%2C%22name%22%3A%22mango%22%2C%22page_verification%22%3Afalse%2C%22strict_locator%22%3A%22css%3Ddiv.FPdoLc.VlcLAe%20%3E%20center%20%3E%20input%5Bname%3D%5C%22btnK%5C%22%5D%22%7D%5D%7D%5D"
     }
 
     PO*/
 
     
-    private By test = By.name("name=btnK");  
+    private By mango = By.name("name=btnK");  
       
 
-    public RemoteWebElement getTest(){
-        return driver.findElement(test);
+    public RemoteWebElement getMango(){
+        return driver.findElement(mango);
     }      
 
     //PAGE-OBJECT-END
+
 
 
 
